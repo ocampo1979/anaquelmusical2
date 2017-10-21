fdcc7bb1829d63ce868459b6532a2db6600af632
ocampo1979 <bato73@hotmail.com>
2017-10-20 22:22:57 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'Biblioteca de Discos en LP - Anaquel Musical';
+  msg: string = '';
+  discos = [];
+
+  model:any = {};
+  model2:any = {};
+    
+  addDisco():void{
+    var answer = confirm('Estás seguro de querer agregar el LP???');
+    if(answer){
+    this.discos.push(this.model);
+    this.msg = 'Disco Agregado';
+    }
+  }
+
+  deleteDisco(i):void{
+    var answer = confirm('Estás seguro de querer borrar el LP???');
+    if(answer){
+      this.discos.splice(i, 1);
+      this.msg = 'Disco Eliminado';
+    }
+  }
+  
+  closeAlert():void{
+    this.msg = '';
+  }
 }
