81289d486d6960abf3ef3d35d4c1404fe7846447
ocampo1979 <bato73@hotmail.com>
2017-11-05 14:11:10 -0500

    prueba

--- src/app/disco/disco.component.ts
+++ src/app/disco/disco.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DiscoService } from '../servicios/disco.service';
 
+import { DiscoDataServerService } from './disco-data-server.service';
+import { Discos } from './disco';
+
 @Component({
   selector: 'app-disco',
   templateUrl: './disco.component.html',
