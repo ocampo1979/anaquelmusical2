29a07bc333af960aea4d69e16f4af8c6f2aae8ca
ocampo1979 <bato73@hotmail.com>
2017-11-05 10:28:01 -0500

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { DiscoComponent } from './disco/disco.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiscoComponent
   ],
   imports: [
     BrowserModule,
