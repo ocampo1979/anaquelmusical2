1ccf36ea7cacc93f5f1410cd6ce3a51b7f98e8ed
ocampo1979 <bato73@hotmail.com>
2017-11-05 14:23:17 -0500

    prueba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title: string = 'Biblioteca de Discos en LP - Anaquel Musical';
-  discos = [];
-
-  model:any = {};
-      
-  addDisco():void{
-      this.discos.push(this.model);
-  }
-
-  deleteDisco(i):void{
-      this.discos.splice(i, 1);
-  }
+  title = 'Biblioteca de Discos en LP - Anaquel Musical';
 }
